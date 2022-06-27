e73dd1658a1d008b45d248e4273691ddea1852a5
Tima <timachulkov1@gmail.com>
2022-06-27 22:40:00 +0500

    Добавьте файлы проекта.

---
+++ Connect2.0/Client.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Connect2._0
+{
+    internal class Clientnet
+    {
+        string ip;
+        int port;
+        bool exit = false;
+        int errorCount;
+        string newstring = "\n";
+        Thread threadtcp = null;
+        String responseData = String.Empty;
+        bool connectionState = false;
+        bool active = false;
+        int arrayIter = 0;
+        int[] sendNumbers;
+        int[] saveNumbers;
+        internal bool arrayEmpty = true;
+        byte[] dataget = new byte[256];
+        List<byte> datarecive = new List<byte>();
+        TcpClient tcpClient;
+        NetworkStream netStream;
+
+        internal Clientnet(string newip,int newport)
+        {
+            ip = newip;
+            port = newport;
+
+        }
+        internal void Fillnum(int[] toFill)
+        {
+            sendNumbers = toFill;
+            
+            saveNumbers = new int[sendNumbers.Length];
+            arrayEmpty = false;
+        }
+        internal int[] Takearray()
+        {
+            return saveNumbers;
+        }
+        internal void Start()
+        {
+            Connect();
+            threadtcp = new Thread(tcp);
+            threadtcp.Start();
+            active = true;
+        }
+        internal void Stop()
+        {
+            Disconnect();
+            active = false;
+        }
+        void Connect()
+        {
+            try
+            {
+                tcpClient = new TcpClient(ip, port);
+                netStream = tcpClient.GetStream();
+                connectionState = true;
+            }
+            catch
+            {
+                connectionState = false;
+            }
+        }
+        void Disconnect()
+        {
+            netStream.Close();
+            connectionState = false;
+        }
+        void tcp()
+        {
+            while (active)
+            {
+                if (connectionState)
+                {
+                    if (sendNumbers.Length > 0)
+                    {
+                        try
+                        {
+                            errorCount = 0;
+                                netStream.ReadTimeout = 20000;
+                                byte[] data = System.Text.Encoding.ASCII.GetBytes(Convert.ToString(sendNumbers[0]) + newstring);
+                                netStream.Write(data, 0, data.Length);
+                                Console.WriteLine("Sent: {0}", Convert.ToString(sendNumbers[0]));
+                                
+                                do
+                                {
+
+                                    int tmpByte;
+                                    exit = false;
+                                    while(exit == false)
+                                    {
+                                        tmpByte = netStream.ReadByte();
+                                        if (tmpByte != -1)
+                                        {
+
+                                            datarecive.Add(Convert.ToByte(tmpByte));
+                                        }
+                                        else
+                                        {
+                                            Thread.Sleep(1000);
+                                            errorCount++;
+                                        }
+
+
+                                        if (datarecive.Count() > 5)
+                                        {
+                                            if (datarecive[datarecive.Count()-1] == 10 && datarecive[datarecive.Count() - 2] == 13 && datarecive[datarecive.Count() - 3] == 10 && datarecive[datarecive.Count() - 4] == 13)
+                                            {
+                                                exit = true;
+                                            }
+                                        }
+
+                                        if (errorCount > 150)
+                                        {
+                                            throw new InvalidOperationException("Infinit loop");
+                                        }
+                                        Thread.Sleep(10);
+                                    }
+                                    while (!exit);
+
+                                    for (int i = 0; i < datarecive.Count(); i++)
+                                    {
+                                        dataget[i] = datarecive[i];
+                                    }
+                                    Encoding encoding = Encoding.GetEncoding(20866);
+                                    responseData = encoding.GetString(dataget, 0, dataget.Length);
+                                    responseData = Regex.Match(responseData, @"\d+").Value;
+                                    Thread.Sleep(100);
+
+                                }
+                                while (responseData.Length < 7);
+                                Console.WriteLine("Received: {0}", responseData);
+                                saveNumbers[arrayIter] = Convert.ToInt32(responseData);
+                                arrayIter++;
+                                sendNumbers = sendNumbers.Skip(1).ToArray();
+
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine(ex);
+                            Connect();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            Connect();
+
+                        }
+                        finally
+                        {
+                            datarecive.Clear();
+                        }
+                        arrayEmpty = false;
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                        arrayEmpty = true;
+                    }
+                }
+                else
+                {
+                    Connect();
+                }
+                Thread.Sleep(1);
+            }
+            Disconnect();
+        }
+    }
+}
---
+++ Connect2.0/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Connect2._0
+{
+    class Program
+    {
+        static List<int> array = new List<int>();
+
+        static void Main(string[] args)
+        {
+            int copyLenth = 0;
+            int arraySize = 1;
+            int workThreads = 1;
+            Console.WriteLine("Запишите колличество потоков");
+            workThreads =Convert.ToInt32(Console.ReadLine()); 
+            List<Clientnet> clientList = new List<Clientnet>();
+            double median = 0;
+            string host = "88.212.241.115";
+            double unixTimestamp = 1380619079;
+            int port = UnixTimeStampToYear(unixTimestamp);
+            for (int i = 0; i < workThreads; i++)
+            {
+                clientList.Add(new Clientnet(host, port));
+            }
+            int[] arr = Enumerable.Range(1, arraySize).ToArray();
+            if (arr.Length % workThreads > 0)
+            {
+                int num = 0;
+                int sizeOfnewArray = arr.Length / workThreads;
+                int[] buffer;
+                for (int i = 0; i < arr.Length; i += sizeOfnewArray)
+                {
+                    if(num != workThreads-1)
+                    {
+                        buffer = new int[sizeOfnewArray];
+                        Array.Copy(arr, i, buffer, 0, sizeOfnewArray);
+                        clientList[num].Fillnum(buffer);
+                        num++;
+                    }
+                    else
+                    {
+                        buffer = new int[arr.Length - i];
+                        Array.Copy(arr, i, buffer, 0, arr.Length - i);
+                        clientList[num].Fillnum(buffer);
+                        break;
+                    }
+                }
+            }
+            else
+            {   
+                int num = 0;
+                int sizeOfnewArray = arr.Length / workThreads;
+                int[] buffer;
+                for (int i = 0; i < arr.Length; i += sizeOfnewArray)
+                {
+                    buffer = new int[sizeOfnewArray];
+                    Array.Copy(arr, i, buffer, 0, sizeOfnewArray);
+                    clientList[num].Fillnum(buffer);
+                    num++;
+                }
+            }
+            for (int i = 0; i < workThreads; i++)
+            {
+                clientList[i].Start();
+            };
+            int[] anserArr = new int[arr.Length];
+            List<bool> check = new List<bool>(new bool[clientList.Count()]);
+            do
+            {
+                for (int i = 0; i < clientList.Count(); i++)
+                {
+                    check[i] = clientList[i].arrayEmpty;
+                }
+                Thread.Sleep(1);
+            }
+            while (!check.All(c => c == true));
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < workThreads; i++)
+            {
+                clientList[i].Takearray().CopyTo(anserArr, copyLenth);
+                copyLenth = clientList[i].Takearray().Length + copyLenth;
+            }
+            median = CalculateMedian(anserArr);
+            sendtcp(host, port, Convert.ToString(median));
+            Console.ReadKey();
+
+        }
+        public static void sendtcp(string ip, int port, string send)
+        {
+            String responseData = String.Empty;
+            List<byte> datarecive = new List<byte>();
+            byte[] dataget = new byte[256];
+            TcpClient tcpClient = new TcpClient(ip, port);
+            string response = "Check " + send + "\n";
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(response);
+            NetworkStream netStream = tcpClient.GetStream();
+            netStream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent: {0}", response);
+            int[] read = new int[2];
+            read[0] = 0;
+
+            do
+            {
+                    read[1] = read[0];
+                    read[0] = netStream.ReadByte();
+                    datarecive.Add(Convert.ToByte(read[0]));
+            }
+            while (read[0] != 10 || read[1] != 13);
+            for (int i = 0; i < datarecive.Count(); i++)
+            {
+                dataget[i] = datarecive[i];
+            }
+            Encoding encoding = Encoding.GetEncoding(20866);
+            responseData = encoding.GetString(dataget, 0, dataget.Length);
+            Console.Write("Received: {0}", responseData);
+        }
+
+        public static double CalculateMedian(int[] Arr)
+        {
+            double median;
+            Array.Sort(Arr);
+            if (Arr.Length % 2 == 0)
+            {
+                int medianIndex1 = Arr.Length / 2;
+                int medianIndex2 = Arr.Length / 2 + 1;
+                int number1 = Arr[medianIndex1 - 1];
+                int number2 = Arr[medianIndex2 - 1];
+                median = (number1 + number2) / 2.00;
+            }
+            else
+            {
+                int medianIndex = ((Arr.Length - 1) / 2) + 1;
+                median = Arr[medianIndex - 1];
+            }
+            return median;
+        }
+
+        public static int UnixTimeStampToYear(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime.Year;
+        }
+
+       
+    }
+}
